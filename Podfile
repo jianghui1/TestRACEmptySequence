4ecc2c019f51ed64ac6405d84c2a682d3ec38538
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-15 18:16:11 +0800

    测试用例提交

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform:ios, '8.0'
+
+target 'TestRACEmptySequenceTests' do
+pod 'ReactiveCocoa', '2.5'
+end
--- TestRACEmptySequenceTests/TestRACEmptySequenceTests.m
+++ TestRACEmptySequenceTests/TestRACEmptySequenceTests.m
@@ -8,32 +8,54 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+#import <RACEmptySequence.h>
+
 @interface TestRACEmptySequenceTests : XCTestCase
 
 @end
 
 @implementation TestRACEmptySequenceTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)test_empty
+{
+    RACEmptySequence *eSequence = [RACEmptySequence empty];
+    RACEmptySequence *eSequence1 = [RACEmptySequence empty];
+    
+    NSLog(@"empty -- %@ -- %@", eSequence, eSequence1);
+    
+    // 打印日志；
+    /*
+     2018-08-15 17:27:51.484859+0800 TestRACEmptySequence[84787:15354391] empty -- <RACEmptySequence: 0x604000016500>{ name =  } -- <RACEmptySequence: 0x604000016500>{ name =  }
+     */
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)test_head_tail
+{
+    RACEmptySequence *sequence = [RACEmptySequence empty];
+    NSLog(@"head_tail -- %@ -- %@", sequence.head, sequence.tail);
+    
+    // 打印日志：
+    /*
+     2018-08-15 17:41:17.702545+0800 TestRACEmptySequence[85320:15393534] head_tail -- (null) -- (null)
+     */
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)test_bind
+{
+    RACEmptySequence *eSequence = [RACEmptySequence empty];
+    
+    NSLog(@"bind -- %@", [eSequence bind:^RACStreamBindBlock{
+        return ^(id value, BOOL *stop) {
+            return [RACStream empty];
+        };
+    }]);
+    
+    // 打印日志：
+    /*
+     2018-08-15 18:12:00.895692+0800 TestRACEmptySequence[86589:15487073] bind -- <RACEmptySequence: 0x60000000a8f0>{ name =  }
+     */
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
